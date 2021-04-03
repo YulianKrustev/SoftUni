f14b58c93eec09ee07b6df88d1346f3eee3bcd32
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-03 15:09:51 +0300

    Add FinalExam

---
+++ 02_C# Fundamentals/FinalExamLab/EmojiDetector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex digits = new Regex(@"\d");
+            MatchCollection digitsInText = digits.Matches(input);
+            BigInteger coolNumber = 1;
+
+            foreach (Match number in digitsInText)
+            {
+                coolNumber *= int.Parse(number.ToString());
+            }
+
+            Regex emojiFinder = new Regex(@"([*][*]|[\:][\:])([A-Z][a-z]{2,})\1");
+            MatchCollection emojisInText = emojiFinder.Matches(input);
+
+            Console.WriteLine($"Cool threshold: {coolNumber}");
+            Console.WriteLine($"{emojisInText.Count} emojis found in the text. The cool ones are:");
+
+            foreach (Match emoji in emojisInText)
+            {
+                int points = 0;
+
+                foreach (var symbol in emoji.Groups[2].Value)
+                {
+                    points += char.Parse(symbol.ToString());
+                }
+
+                if (points > coolNumber)
+                {
+                    Console.WriteLine(emoji);
+                }
+            }
+        }
+    }
+}
---
+++ 02_C# Fundamentals/FinalExamLab/FinalExamLab/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace FinalExamLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string rawKey = Console.ReadLine();
+
+            while (rawKey != "Generate")
+            {
+                string[] command = rawKey.Split(">>>");
+
+                if (command[0] == "Contains")
+                {
+                    if (input.Contains(command[1]))
+                    {
+                        Console.WriteLine($"{input} contains {command[1]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (command[0] == "Flip")
+                {
+                    int startIndex = int.Parse(command[2]);
+                    int endIndex = int.Parse(command[3]);
+                    string flipedString = input.Substring(startIndex, endIndex - startIndex);
+
+                    if (command[1] == "Upper")
+                    {
+                        flipedString = flipedString.ToUpper();
+                    }
+                    else if (command[1] == "Lower")
+                    {
+                        flipedString = flipedString.ToLower();
+                    }
+
+                    input = input.Replace(input.Substring(startIndex, endIndex - startIndex), flipedString);
+                    Console.WriteLine(input);
+                }
+                else if (command[0] == "Slice")
+                {
+                    int startIndex = int.Parse(command[1]);
+                    int endIndex = int.Parse(command[2]) - 1;
+                    input = input.Remove(startIndex, endIndex - startIndex +1);
+                    Console.WriteLine(input);
+                }
+
+                rawKey = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {input}");
+        }
+    }
+}
