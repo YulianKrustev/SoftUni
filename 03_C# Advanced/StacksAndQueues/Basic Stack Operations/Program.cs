49f89007e4b5eac5fcf5401531e6baabe0cac908
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-12 16:04:37 +0300

    Start a new course

---
+++ 03_C# Advanced/StacksAndQueues/Basic Stack Operations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int numberOfDigits = int.Parse(tokens[0]);
+            int numbersToPop = int.Parse(tokens[1]);
+            int specialNumber = int.Parse(tokens[2]);
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> digits = new Stack<int>(input);
+
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                digits.Pop();
+            }
+
+            if (digits.Contains(specialNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else if(digits.Count < 1)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                int smallest = int.MaxValue;
+                int currentNumber = 0;
+
+                while (digits.Count != 0)
+                {
+                    currentNumber = digits.Pop();
+
+                    if (currentNumber < smallest)
+                    {
+                        smallest = currentNumber;
+                    }
+                }
+
+                Console.WriteLine(smallest);
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/StacksAndQueues/Maximum and Minimum Element/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (tokens[0] == 1)
+                {
+                    numbers.Push(tokens[1]);
+                }
+                else if (tokens[0] == 2)
+                {
+                    numbers.Pop();
+                }
+                else if (tokens[0] == 3)
+                {
+                    Console.WriteLine(numbers.Max());
+                }
+                else if (tokens[0] == 4)
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+
+            Console.WriteLine(string.Join(",", numbers));
+        }
+    }
+}
