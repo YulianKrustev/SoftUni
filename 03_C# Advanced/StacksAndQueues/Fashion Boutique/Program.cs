85cab3295c4f40734785409485a4bcacbe6a53ac
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-12 21:08:49 +0300

    new problem solved

---
+++ 03_C# Advanced/StacksAndQueues/Fashion Boutique/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> box = new Stack<int>(clothes);
+            int sizeOfRack = int.Parse(Console.ReadLine());
+            int currentRack = 0;
+            int counter = 1;
+
+            while (box.Count > 0)
+            {
+                if (box.Peek() + currentRack <= sizeOfRack)
+                {
+                    currentRack += box.Pop();
+                }
+                else
+                {
+                    counter++;
+                    currentRack = 0;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 03_C# Advanced/StacksAndQueues/Songs Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> songList = new Queue<string>(songs);
+
+            while (songList.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command.StartsWith("Add"))
+                {
+                    string songName = command.Substring(4);
+
+                    if (songList.Contains(songName))
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                    else
+                    {
+                        songList.Enqueue(songName);
+                    }
+                }
+                else if (command == "Play") 
+                {
+                    songList.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(", ", songList));
+                }
+
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
