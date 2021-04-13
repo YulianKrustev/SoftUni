b987920f66e4093b88d2bc294b7647cf93da0bfc
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-13 21:00:11 +0300

    new problem solved

---
+++ 03_C# Advanced/StacksAndQueues/Truck Tour/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pumps = int.Parse(Console.ReadLine());
+            Queue<string> truckTour = new Queue<string>();
+            int startIndex = 0;
+            int liters = 0;
+
+            for (int i = 0; i < pumps; i++)
+            {
+                truckTour.Enqueue(Console.ReadLine());
+            }
+
+            for (int i = 0; i < pumps; i++)
+            {
+                string tokens = truckTour.Dequeue();
+                string[] current = tokens.Split();
+                liters += int.Parse(current[0]) - int.Parse(current[1]);
+
+                if (liters < 0)
+                {
+                    startIndex = i + 1;
+                    liters = 0;
+                }
+
+                truckTour.Enqueue(tokens);
+            }
+
+            Console.WriteLine(startIndex);
+
+        }
+    }
+}
