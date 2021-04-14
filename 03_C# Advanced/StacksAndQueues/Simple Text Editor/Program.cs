f5664d87ba5174c78f3a14f0979b8f0c844e5476
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-14 19:20:55 +0300

    new problem solved

--- 03_C# Advanced/StacksAndQueues/Simple Text Editor/Program.cs
+++ 03_C# Advanced/StacksAndQueues/Simple Text Editor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Simple_Text_Editor
 {
@@ -6,7 +8,36 @@ namespace Simple_Text_Editor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StringBuilder builder = new StringBuilder();
+            Stack<string> undoCommand = new Stack<string>();
+            int count = int.Parse(Console.ReadLine());
+            undoCommand.Push(builder.ToString());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                int command = int.Parse(tokens[0]);
+
+                switch (command)
+                {
+                    case 1:
+                        builder.Append(tokens[1]);
+                        undoCommand.Push(builder.ToString());
+                        break;
+                    case 2:
+                        builder.Remove(builder.Length - int.Parse(tokens[1]), int.Parse(tokens[1]));
+                        undoCommand.Push(builder.ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine(builder[int.Parse(tokens[1]) - 1]);
+                        break;
+                    case 4:
+                        undoCommand.Pop();
+                        builder = new StringBuilder();
+                        builder.Append(undoCommand.Peek());
+                        break;
+                }
+            }
         }
     }
 }
