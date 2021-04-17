839af7a0875640ddb043415a3574702a937e54b4
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-17 11:11:37 +0300

    new

---
+++ 03_C# Advanced/StacksAndQueues/HotPatato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPatato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(input);
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (queue.Count != 1)
+            {
+                counter++;
+                string current = queue.Peek();
+
+                if (counter % n == 0)
+                {
+                    Console.WriteLine($"Removed {queue.Dequeue()}");
+                }
+                else
+                {
+                    queue.Dequeue();
+                    queue.Enqueue(current);
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
