454d7b4255de430f98c525da3af1519a8ecdac2a
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-19 13:24:14 +0300

    new problem solved

--- 03_C# Advanced/StacksAndQueues/Cups and bottles/Program.cs
+++ 03_C# Advanced/StacksAndQueues/Cups and bottles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Cups_and_bottles
@@ -7,9 +8,52 @@ namespace Cups_and_bottles
     {
         static void Main(string[] args)
         {
-            int[] cups = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] filledBottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] cups = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] filledBottles = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            Queue<int> cupsList = new Queue<int>(cups);
+            Stack<int> bottlesList = new Stack<int>(filledBottles);
+            int weastedWater = 0;
+
+            while (cupsList.Any() && bottlesList.Any())
+            {
+                int currentCup = cupsList.Dequeue();
+                int currentBottle = bottlesList.Pop();
+
+                while (currentCup > currentBottle)
+                {
+                    currentCup -= currentBottle;
+                    currentBottle = bottlesList.Pop();
+
+                    if (currentCup < 0)
+                    {
+                        weastedWater -= currentCup;
+                    }
+                }
+
+                if (currentBottle >= currentCup)
+                {
+                    weastedWater += currentBottle - currentCup;
+                }
+            }
+
+            if (cupsList.Any())
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupsList)}");
+                Console.WriteLine($"Wasted litters of water: {weastedWater}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottlesList)}");
+                Console.WriteLine($"Wasted litters of water: {weastedWater}");
+            }
+            
 
         }
     }
---
+++ 03_C# Advanced/StacksAndQueues/TrafficJam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            string car = Console.ReadLine();
+            int counter = 0;
+
+            while (car != "end")
+            {
+                if (car == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Any())
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            counter++;
+                        }                        
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(car);
+                }
+
+                car = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
