a9f8b49b067a2b8f5182051d50178faa181eabe7
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-19 19:31:21 +0300

    new problem solved

---
+++ 03_C# Advanced/Multidimensional Arrays/Multidimensional Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Multidimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console
+                .ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows ; row++)
+            {
+                int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
