7c8c4a9794f810edf07ac23a1c5fc352a06ef86b
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-21 12:28:27 +0300

    new problem solved

--- 03_C# Advanced/Multidimensional Arrays/SquareWithMaximumSum/Program.cs
+++ 03_C# Advanced/Multidimensional Arrays/SquareWithMaximumSum/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Linq;
 
 namespace SquareWithMaximumSum
 {
@@ -6,7 +8,51 @@ namespace SquareWithMaximumSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            int maxSum = 0;
+            int rowToPrint = 0;
+            int colToPrint = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int currentSum = 0;
+
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row + 1 < rows && col + 1 < cols)
+                    {
+                        currentSum = matrix[row, col] + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row, col + 1];
+
+                        if (currentSum > maxSum)
+                        {
+                            maxSum = currentSum;
+                            rowToPrint = row;
+                            colToPrint = col;
+                        }
+                        
+                    }
+                    
+                }
+
+            }
+
+            Console.WriteLine($"{matrix[rowToPrint, colToPrint]} {matrix[rowToPrint, colToPrint + 1]}");
+            Console.WriteLine($"{matrix[rowToPrint +1, colToPrint]} {matrix[rowToPrint + 1, colToPrint + 1]}");
+            Console.WriteLine(maxSum);
         }
     }
 }
