bbec0815ae77307fdc5e8809c0048772df7eb4a8
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-22 11:20:58 +0300

    new commit

--- 03_C# Advanced/Multidimensional Arrays/PascalTriangle/Program.cs
+++ 03_C# Advanced/Multidimensional Arrays/PascalTriangle/Program.cs
@@ -6,7 +6,39 @@ namespace PascalTriangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] array = new long[rows][];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+
+                array[row] = new long[row + 1];
+
+                for (int col = 0; col < row + 1; col++)
+                {
+                    if (row - 1 >= 0 && col - 1 >= 0 && col + 1 < row + 1)
+                    {
+                        array[row][col] = array[row - 1][col - 1] + array[row - 1][col];
+                    }
+                    else
+                    {
+                        array[row][col] = 1;
+                    }
+                     
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < row + 1; col++)
+                {
+                    Console.Write(array[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
