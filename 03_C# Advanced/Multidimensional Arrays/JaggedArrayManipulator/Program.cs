0b2d4711459bc61eb529f9c96a64dc52e56e1e24
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-24 20:19:36 +0300

    new task

---
+++ 03_C# Advanced/Multidimensional Arrays/JaggedArrayManipulator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            double[][] matrix = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                double[] inputLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+                matrix[row] = inputLine;
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row+1].Length)
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] *= 2; 
+                        matrix[row + 1][col] *= 2; 
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] /= 2;                      
+                    }
+
+                    for (int col = 0; col < matrix[row + 1].Length; col++)
+                    {
+                        matrix[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+
+                if (command == "Add")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+
+                    if (row >= 0 && row < rows && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] += value;
+                    }                   
+                }
+                else if (command == "Subtract")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+
+                    if (row >= 0 && row < rows && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(string.Join(" ", matrix[row]));
+            }
+            
+        }
+    }
+}
