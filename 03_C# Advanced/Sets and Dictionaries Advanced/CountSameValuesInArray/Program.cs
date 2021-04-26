d422185978993c2c531aa8294454378024b5b8df
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-26 16:20:18 +0300

    new commit

---
+++ 03_C# Advanced/Sets and Dictionaries Advanced/CountSameValuesInArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> databaase = new Dictionary<string, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!databaase.ContainsKey(input[i]))
+                {
+                    databaase.Add(input[i], 0);
+                }
+
+                databaase[input[i]]++;
+            }
+
+            foreach (var item in databaase)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
