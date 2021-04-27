17a43844bb8497ef23617e53d8ab865bef8fb1e3
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-27 13:57:09 +0300

    new commit

---
+++ 03_C# Advanced/Sets and Dictionaries Advanced/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int continents = int.Parse(Console.ReadLine());
+
+            var database = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < continents; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (database.ContainsKey(continent) == false)
+                {
+                    database.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (database[continent].ContainsKey(country) == false)
+                {
+                    database[continent].Add(country, new List<string>());
+                }
+
+                database[continent][country].Add(city);
+            }
+
+            foreach (var continent in database)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/Sets and Dictionaries Advanced/ProductShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+
+            var databese = new Dictionary<string, Dictionary<string, double>>();
+
+            while (input != "Revision")
+            {
+                string[] tokens = input.Split(", ");
+                string store = tokens[0];
+                string product = tokens[1];
+                double price = double.Parse(tokens[2]);
+
+                if (databese.ContainsKey(store) == false)
+                {
+                    databese.Add(store, new Dictionary<string, double>());
+                }
+
+                databese[store].Add(product, price);
+                input = Console.ReadLine();
+            }
+
+            foreach (var store in databese.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{store.Key}->");
+
+                foreach (var product in store.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
