138d4a67ef4073bf064d3b53aafa27f6b92592ea
YulianKrustev <juliankrustev2018@gmail.com>
2021-04-29 02:21:26 +0300

    new commit

---
+++ 03_C# Advanced/Sets and Dictionaries Advanced/EvenTimes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumber = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> database = new Dictionary<int, int>();
+
+
+            for (int i = 0; i < countNumber; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+
+                if (!database.ContainsKey(current))
+                {
+                    database.Add(current, 0);
+                }
+
+                database[current]++;
+            }
+
+            foreach (var item in database)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
