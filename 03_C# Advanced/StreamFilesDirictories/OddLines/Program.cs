ddb3ea41d221849977cf44760d502eebdc6ed52e
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-09 17:09:52 +0300

    new commit

---
+++ 03_C# Advanced/StreamFilesDirictories/OddLines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            using (var input = new StreamReader("input.txt"))
+            {
+                while (input.EndOfStream == false)
+                {
+                    counter++;
+                    string currentLine = input.ReadLine();
+
+                    if (counter % 2 == 0)
+                    {
+                        Console.WriteLine(currentLine);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/StreamFilesDirictories/StreamFilesDirictories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamFilesDirictories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
