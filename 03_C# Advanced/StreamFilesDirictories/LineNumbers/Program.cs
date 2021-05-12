a94ec0435d5752410a87f429b61e49ee4a1e9589
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-12 21:42:52 +0300

    new problem solved

---
+++ 03_C# Advanced/StreamFilesDirictories/LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "input.txt";
+            string output = "output.txt";
+
+            RerightFilesWithNumbers(inputFile, output);
+        }
+
+        private static void RerightFilesWithNumbers(string inputFile, string output)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                using (StreamWriter sw = new StreamWriter(output))
+                {
+                    int count = 1;
+
+                    while (sr.EndOfStream == false)
+                    {
+                        string current = sr.ReadLine();
+                        sw.WriteLine($"{count}. {current}");
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
