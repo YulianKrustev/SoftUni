8bbdcfe490dd161c4cc4055a27fde7080bd7e68b
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-14 11:22:35 +0300

    new commit

---
+++ 03_C# Advanced/StreamFilesDirictories/MergeTextFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace MergeTextFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFileOne = "input1.txt";
+            string inputFileTwo = "input2.txt";
+
+            MetgeText(inputFileOne, inputFileTwo);
+        }
+
+        private static void MetgeText(string inputFileOne, string inputFileTwo)
+        {
+            using (StreamReader sr1 = new StreamReader(inputFileOne))
+            {
+                using (StreamReader sr2 = new StreamReader(inputFileTwo))
+                {
+                    while (sr1.EndOfStream == false || sr2.EndOfStream == false)
+                    {
+                        using (StreamWriter sr = new StreamWriter("output.txt", true))
+                        {
+                            if (sr1.EndOfStream == false)
+                            {
+                                string current1 = sr1.ReadLine();
+                                sr.WriteLine(current1);
+                            }
+                            if (sr2.EndOfStream == false)
+                            {
+                                string current2 = sr2.ReadLine();
+                                sr.WriteLine(current2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
