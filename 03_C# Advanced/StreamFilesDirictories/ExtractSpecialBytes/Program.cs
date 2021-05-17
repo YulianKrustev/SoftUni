e10f7d89860bec026d2b9e5cd5c8f9ec645be80c
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-17 22:40:15 +0300

    new commit

---
+++ 03_C# Advanced/StreamFilesDirictories/ExtractSpecialBytes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ExtractSpecialBytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bytes = File.ReadAllText("bytes.txt").Split();
+            Console.WriteLine(bytes[0]);
+
+    
+        }
+    }
+}
