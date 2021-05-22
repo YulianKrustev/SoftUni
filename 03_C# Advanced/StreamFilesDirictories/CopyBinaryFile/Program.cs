83a9f68f8530cb26a5f0599d1f59ad0bfe319068
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-22 11:59:05 +0300

    new commit

---
+++ 03_C# Advanced/StreamFilesDirictories/CopyBinaryFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream reader = new FileStream(@"..\..\..\copyMe.png", FileMode.Open))
+            using (FileStream writer = new FileStream(@"..\..\..\newFile.png", FileMode.Create))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    writer.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
