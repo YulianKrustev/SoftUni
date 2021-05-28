c115afcce66daffc060e9e7ee6d99a0901e4e9e9
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-28 12:48:06 +0300

    new commit

---
+++ 03_C# Advanced/StreamFilesDirictories/ZipAndExtract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"d:\softuni";
+            string zipPath = @"d:\softuni.zip";
+            string extractPath = @"d:\softuniE";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
