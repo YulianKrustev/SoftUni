ac64404ba928de0480bb3ca9ec31bcf923c30548
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-31 19:34:46 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/CustomMinFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> MinNumber = numbers =>
+            {
+                int minNumber = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                }
+
+                return minNumber;
+            };
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int result = MinNumber(numbers);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ 03_C# Advanced/FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> Print = Console.WriteLine;
+
+            Console.ReadLine().Split().ToList().ForEach(x => Print($"Sir {x}"));
+        }
+    }
+}
