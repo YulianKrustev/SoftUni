f2e070edb1efa5392e82f95df5fb32e25899e4fb
YulianKrustev <juliankrustev2018@gmail.com>
2021-05-31 17:28:02 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> Print = Console.WriteLine;
+            Console.ReadLine()
+                .Split()
+                .ToList().
+                ForEach(x => Print(x));
+        }
+    }
+}
