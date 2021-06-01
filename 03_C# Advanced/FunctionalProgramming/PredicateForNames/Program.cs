bd43892a8d2388516d356e38609f706fc05d3977
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-01 12:00:01 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/PredicateForNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfName = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, bool> checkLengthFunc = x => x.Length <= lengthOfName;
+            names.Where(checkLengthFunc).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
