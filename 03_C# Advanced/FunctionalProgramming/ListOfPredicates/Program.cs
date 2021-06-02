5d4e04f9a099b78cbe26a8a9b728d679ea5af52d
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-02 11:57:20 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/ListOfPredicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Distinct()
+                .ToArray();
+
+            List<int> numbers = new List<int>();
+            Func<int, int, bool> checker = (x, y) => x % y != 0;
+
+            for (int i = 1; i <= range; i++)
+            {
+                bool isValid = true;
+
+                foreach (var number in dividers)
+                {
+                    if (checker(i, number))
+                    {
+                        isValid = false;
+                    }
+                }
+
+                if (isValid == true)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
