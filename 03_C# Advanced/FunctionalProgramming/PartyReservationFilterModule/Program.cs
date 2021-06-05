b3e490a3ad90c39ddb6af16d63b8ada49ecf96a4
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-05 12:21:30 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/PartyReservationFilterModule/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine().Split().ToList();
+            List<string> filters = new List<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Print")
+            {
+
+                string[] commands = input.Split(";");
+
+                if (commands[0] == "Add filter")
+                {
+                    filters.Add($"{commands[1]} {commands[2]}");
+                }
+                else if (commands[0] == "Remove filter")
+                {
+                    filters.Remove($"{commands[1]} {commands[2]}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                var commands = filter.Split();
+
+                if (commands[0] == "Starts")
+                {
+                    guests = guests.Where(p => !p.StartsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Ends")
+                {
+                    guests = guests.Where(p => !p.EndsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Length")
+                {
+                    guests = guests.Where(p => p.Length != int.Parse(commands[1])).ToList();
+                }
+                else if (commands[0] == "Contains")
+                {
+                    guests = guests.Where(p => !p.Contains(commands[1])).ToList();
+                }
+            }
+
+            if (guests.Any())
+            {
+                Console.WriteLine(string.Join(" ", guests));
+            }
+        }
+    }
+}
--- 03_C# Advanced/FunctionalProgramming/PredicateParty!/Program.cs
+++ 03_C# Advanced/FunctionalProgramming/PredicateParty!/Program.cs
@@ -16,21 +16,24 @@ namespace PredicateParty_
                 string action = command[0];
                 string condition = command[1];
 
-                if (action == "Remove")
+                 if (action == "Remove")
                 {
                     for (int i = 0; i < people.Count; i++)
                     {
                         if (condition == "StartsWith" && people[i].StartsWith(command[2]))
                         {
                             people.Remove(people[i]);
+                            i--;
                         }
                         else if (condition == "EndsWith" && people[i].EndsWith(command[2]))
                         {
                             people.Remove(people[i]);
+                            i--;
                         }
                         else if (condition == "Length" && people[i].Length == int.Parse(command[2]))
                         {
                             people.Remove(people[i]);
+                            i--;
                         }
                     }
                     
