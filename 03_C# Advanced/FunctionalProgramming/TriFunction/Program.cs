23985ed61667202248ed20445692a300e69eef74
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-05 12:33:49 +0300

    new commit

---
+++ 03_C# Advanced/FunctionalProgramming/TriFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> lengthCheck = (name, sum) => name.Sum(x => x) >= sum;
+
+            Console.WriteLine(names.FirstOrDefault(x => lengthCheck(x, sum)));
+        }
+    }
+}
