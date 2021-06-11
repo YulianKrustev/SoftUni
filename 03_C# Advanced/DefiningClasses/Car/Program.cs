d9d52f56746b97d070582a2482c4831f234dc50e
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-11 19:55:49 +0300

    new commit

---
+++ 03_C# Advanced/DefiningClasses/Car/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Program
+    {
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public string Year { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Polo";
+            car.Year = "2017";
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
