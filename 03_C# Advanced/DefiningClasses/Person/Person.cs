4bc5e6026db8c7e9ef967bcb655fd01b33479885
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-14 14:36:53 +0300

    new commit

---
+++ 03_C# Advanced/DefiningClasses/Person/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public Person()
+        {
+            this.Age = 1;
+            this.Name = "No name";
+        }
+
+        public Person(int age)
+            : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            :this(age)
+        {
+            this.Name = name;
+        }
+        public int Age
+        {
+            get { return age; }
+            set { Age = age; }
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { Name = name; }
+
+        }
+    }
+}
