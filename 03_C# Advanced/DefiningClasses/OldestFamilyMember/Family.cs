1837bc531ba4cbb8804240cd5bc0e71dfb4f9cea
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-15 13:47:05 +0300

    new commit

---
+++ 03_C# Advanced/DefiningClasses/OldestFamilyMember/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        List<Person> family;
+
+        public Family()
+        {
+            this.family = new List<Person>();
+        }
+        public void AddMember(Person person)
+        {
+            this.family.Add(person);
+        }
+
+        public Person GetOlderMember()
+        {
+            return family.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
---
+++ 03_C# Advanced/DefiningClasses/OldestFamilyMember/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+
+            }
+
+            Person oldest = family.GetOlderMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
