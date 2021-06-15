9b421e055a4be3292bbd8d18a7bed024f789bfd8
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-15 22:33:16 +0300

    new commit

---
+++ 03_C# Advanced/DefiningClasses/OpinionPoll/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int countPeople = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < countPeople; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+
+            foreach (Person person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
