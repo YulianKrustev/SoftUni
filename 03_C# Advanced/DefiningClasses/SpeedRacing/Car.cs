d84736742fb096a9c9e9cb5141bff3c704456e8f
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-16 12:40:23 +0300

    new commit

---
+++ 03_C# Advanced/DefiningClasses/SpeedRacing/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string model;
+        public double fuelAmount;
+        public double consumptionPerKm;
+        public double distance;
+
+        public Car(string model, double fuelAmount, double consumptionPerKm)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.consumptionPerKm = consumptionPerKm;
+        }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = distance * this.consumptionPerKm;
+
+            if (neededFuel <= this.fuelAmount)
+            {
+                this.distance += distance;
+                this.fuelAmount -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/DefiningClasses/SpeedRacing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < countOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double consumptionPerKm = double.Parse(input[2]);
+
+                Car car = new Car(model, fuelAmount, consumptionPerKm);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] drive = command.Split();
+                string model = drive[1];
+                double distance = double.Parse(drive[2]);
+
+                cars.Find(x => x.model == model).Drive(distance);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.model} {car.fuelAmount:f2} {car.distance}");
+            }
+        }
+    }
+}
