154a779ddbc1ff4009e80c1e67ac580d989f3e8d
YulianKrustev <juliankrustev2018@gmail.com>
2021-06-23 13:13:04 +0300

    new

---
+++ 03_C# Advanced/WorkShop/WorkShop/CustomList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkShop
+{
+    class CustomList
+    {
+        private int[] items;
+        private const int Initialcapacity = 2;
+
+        public CustomList()
+        {
+            this.items = new int[Initialcapacity];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                ValideteIndex(index);
+                return items[index]; 
+            }
+            set 
+            {
+                ValideteIndex(index);
+                items[index] = value; 
+            }
+        }
+        public void Add(int i)
+        {
+            Resize();
+            items[Count] = i;
+            Count++;
+        }
+
+        private void Shrink()
+        {
+            if (Count * 4 >= items.Length)
+            {
+                return;
+            }
+
+            int[] temp = new int[this.items.Length / 2];
+            Array.Copy(items, temp, Count);
+            items = temp;
+        }
+
+        private void Resize()
+        {
+            if (items.Length < Count)
+            {
+                return;
+            }
+
+            int[] temp = new int[this.items.Length * 2];
+            Array.Copy(items, temp, items.Length);
+            items = temp;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            items[items.Length-1] = default;
+        }
+
+        public int RemoveAt(int index)
+        {
+            ValideteIndex(index);
+            int element = items[index];
+
+            Shift(index);
+            Count--;
+            Shrink();
+
+            return element;
+        }
+
+        private void ValideteIndex(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ 03_C# Advanced/WorkShop/WorkShop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WorkShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(i);
+            }
+
+            list.RemoveAt(1);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
