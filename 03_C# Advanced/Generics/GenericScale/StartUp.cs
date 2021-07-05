299ba8983d89582961e1b40219bf547f757e6481
YulianKrustev <juliankrustev2018@gmail.com>
2021-07-05 20:08:38 +0300

    new commit

---
+++ 03_C# Advanced/Generics/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var equal = new EqualityScale<int>(4, 2);
+            Console.WriteLine(equal.AreEqual());
+        }
+    }
+}
