60da019fd68c686b401d6afa3a9b8f0d0d34e199
YulianKrustev <juliankrustev2018@gmail.com>
2021-07-06 21:34:02 +0300

    update

---
+++ 03_C# Advanced/Generics/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private List<T> boxCollection;
+
+        public Box()
+        {
+            boxCollection = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            boxCollection.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in boxCollection)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 03_C# Advanced/Generics/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> list = new Box<int>();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                list.Add(current);
+            }
+
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
