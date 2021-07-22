933d685e41b88337ba71a062c95fb0b030dd1fe5
YulianKrustev <juliankrustev2018@gmail.com>
2021-07-23 01:44:24 +0300

    upload new solution

---
+++ 03_C# Advanced/IteratorsAndComparators/IteratorsAndComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> collection;
+        private int index = 0;
+
+        public ListyIterator(T[] elements)
+        {
+            collection = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            if (index < collection.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < collection.Count;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(collection[index]);
+        }
+    }
+}
---
+++ 03_C# Advanced/IteratorsAndComparators/IteratorsAndComparators/ListyIterator/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            var collection = new ListyIterator<string>(input);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "move":
+                        Console.WriteLine(collection.Move());
+                        break;
+                    case "print":
+                        try
+                        {
+                            collection.Print();
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "hasnext":
+                        Console.WriteLine(collection.HasNext()); 
+                            break;
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
