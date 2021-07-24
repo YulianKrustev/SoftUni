0e240098d397a6e60db20ef01ac71adf44fc96d5
YulianKrustev <juliankrustev2018@gmail.com>
2021-07-24 11:50:23 +0300

    new solution

---
+++ 03_C# Advanced/IteratorsAndComparators/IteratorsAndComparators/Froggy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var stones = new Lake(input);
+
+            Console.WriteLine(string.Join(", ", stones));
+        }
+    }
+}
