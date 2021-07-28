136038ff4b4a924b0360f1927f343c578e191612
YulianKrustev <juliankrustev2018@gmail.com>
2021-07-28 23:50:24 +0300

    new solution

---
+++ 03_C# Advanced/ExamPreparation/Bee/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+namespace Bee
+{
+    class Program
+    {
+        static int size = int.Parse(Console.ReadLine());
+        static char[][] beeField = new char[size][];
+        static int beeRow = 0;
+        static int beeCol = 0;
+        static int popollinateFlowers = 0;
+
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                string input = Console.ReadLine();
+                beeField[row] = new char[input.Length];
+
+                for (int col = 0; col < size; col++)
+                {
+                    beeField[row][col] = input[col];
+
+                    if (beeField[row][col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                        beeField[beeRow][beeCol] = '.';
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                switch (command)
+                {
+                    case "up":
+                        if (beeRow - 1 < 0)
+                        {
+                            BeeIsLost();
+                        }
+                        else if (beeField[beeRow-1][beeCol] == 'f')
+                        {
+                            popollinateFlowers++;
+                            beeRow--;
+                            beeField[beeRow][beeCol] = '.';
+                        }
+                        else if (beeField[beeRow - 1][beeCol] == 'O')
+                        {
+                            beeRow--;
+                            beeField[beeRow][beeCol] = '.';
+
+                            if (beeRow - 1 < 0)
+                            {
+                                BeeIsLost();
+                            }
+                            else if (beeField[beeRow - 1][beeCol] == 'f')
+                            {
+                                popollinateFlowers++;
+                                beeRow--;
+                                beeField[beeRow][beeCol] = '.';
+                            }
+                        }
+                        else
+                        {
+                            beeRow--;
+                        }
+                        break;
+                    case "down":
+                        if (beeRow + 1 == size)
+                        {
+                            BeeIsLost();
+                        }
+                        else if (beeField[beeRow + 1][beeCol] == 'f')
+                        {
+                            popollinateFlowers++;
+                            beeRow++;
+                            beeField[beeRow][beeCol] = '.';
+                        }
+                        else if (beeField[beeRow + 1][beeCol] == 'O')
+                        {
+                            beeRow++;
+                            beeField[beeRow][beeCol] = '.';
+
+                            if (beeRow + 1 == size)
+                            {
+                                BeeIsLost();
+                            }
+                            else if (beeField[beeRow + 1][beeCol] == 'f')
+                            {
+                                popollinateFlowers++;
+                                beeRow++;
+                                beeField[beeRow][beeCol] = '.';
+                            }
+                        }
+                        else
+                        {
+                            beeRow++;
+                        }
+                        break;
+                    case "left":
+                        if (beeCol - 1 < 0)
+                        {
+                            BeeIsLost();
+                        }
+                        else if (beeField[beeRow][beeCol - 1] == 'f')
+                        {
+                            popollinateFlowers++;
+                            beeCol--;
+                            beeField[beeRow][beeCol] = '.';
+                        }
+                        else if (beeField[beeRow][beeCol - 1] == 'O')
+                        {
+                            beeCol--;
+                            beeField[beeRow][beeCol] = '.';
+
+                            if (beeCol - 1 < 0)
+                            {
+                                BeeIsLost();
+                            }
+                            else if (beeField[beeRow][beeCol - 1] == 'f')
+                            {
+                                popollinateFlowers++;
+                                beeCol--;
+                                beeField[beeRow][beeCol] = '.';
+                            }
+                        }
+                        else
+                        {
+                            beeCol--;
+                        }
+                        break;
+                    case "right":
+                        if (beeCol + 1 == size)
+                        {
+                            BeeIsLost();
+                        }
+                        else if (beeField[beeRow][beeCol + 1] == 'f')
+                        {
+                            popollinateFlowers++;
+                            beeCol++;
+                            beeField[beeRow][beeCol] = '.';
+                        }
+                        else if (beeField[beeRow][beeCol + 1] == 'O')
+                        {
+                            beeCol++;
+                            beeField[beeRow][beeCol] = '.';
+
+                            if (beeCol + 1 ==size)
+                            {
+                                BeeIsLost();
+                            }
+                            else if (beeField[beeRow][beeCol + 1] == 'f')
+                            {
+                                popollinateFlowers++;
+                                beeCol++;
+                                beeField[beeRow][beeCol] = '.';
+                            }
+                        }
+                        else
+                        {
+                            beeCol++;
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            beeField[beeRow][beeCol] = 'B';
+            if (popollinateFlowers < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - popollinateFlowers} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {popollinateFlowers} flowers!");
+            }
+
+            foreach (var row in beeField)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+        private static void BeeIsLost()
+        {
+            beeField[beeRow][beeCol] = '.';
+            Console.WriteLine("The bee got lost!");
+
+            if (popollinateFlowers < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - popollinateFlowers} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {popollinateFlowers} flowers!");
+            }
+
+            foreach (var row in beeField)
+            {
+                Console.WriteLine(row);
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ 03_C# Advanced/ExamPreparation/FlowerWreaths/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            int storedFlowers = 0;
+            int wreaths = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int currentFlowers = lilies.Pop() + roses.Dequeue();
+
+                if (currentFlowers == 15)
+                {
+                    wreaths++;
+                }
+                else if (currentFlowers < 15)
+                {
+                    storedFlowers += currentFlowers;
+                }
+                else
+                {
+                    while (currentFlowers > 15)
+                    {
+                        currentFlowers -= 2;
+                    }
+
+                    if (currentFlowers == 15)
+                    {
+                        wreaths++;
+                    }
+                    else
+                    {
+                        storedFlowers += currentFlowers;
+                    }
+                }
+            }
+
+            if (storedFlowers > 14 )
+            {
+                wreaths += storedFlowers / 15; 
+            }
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5-wreaths} wreaths more!");
+            }
+        }
+    }
+}
