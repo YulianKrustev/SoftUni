62baa78f4eca735356aa7e5a21fff003b4b38636
YulianKrustev <juliankrustev2018@gmail.com>
2021-08-12 17:15:47 +0300

    new solution

---
+++ 04_C# OOP/InheritanceExercise/Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
