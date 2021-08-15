83134878c6a6b1ef88a235941faeee20fff79728
YulianKrustev <juliankrustev2018@gmail.com>
2021-08-15 13:50:25 +0300

    new solution

--- 04_C# OOP/InheritanceExercise/NeedForSpeed/StartUp.cs
+++ 04_C# OOP/InheritanceExercise/NeedForSpeed/StartUp.cs
@@ -4,7 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            
+            Car car = new Car(12, 33);
+            SportCar sportCar = new SportCar(12, 33);
+
+            car.Drive(10);
+            sportCar.Drive(10);
+
+            System.Console.WriteLine(car.Fuel);
+            System.Console.WriteLine(sportCar.Fuel);
+            ;
         }
     }
 }
