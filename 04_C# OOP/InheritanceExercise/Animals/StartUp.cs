5b8cf41da607385346ba00b8f3392f8da6e7643b
YulianKrustev <juliankrustev2018@gmail.com>
2021-08-17 11:16:35 +0300

    new solution

--- 04_C# OOP/InheritanceExercise/Animals/StartUp.cs
+++ 04_C# OOP/InheritanceExercise/Animals/StartUp.cs
@@ -1,10 +1,70 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> animals = new List<Animal>();
+            string input = Console.ReadLine();
+
+            while (input != "Beast!")
+            {
+                try
+                {
+                    string[] tokens = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string name = tokens[0];
+                    int age = int.Parse(tokens[1]);
+                    string gender;
+
+                    if (input == "Cat")
+                    {
+                        gender = tokens[2];
+                        Cat cat = new Cat(name, age, gender);
+                        animals.Add(cat);
+                    }
+                    else if (input == "Dog")
+                    {
+                        gender = tokens[2];
+                        Dog dog = new Dog(name, age, gender);
+                        animals.Add(dog);
+                    }
+                    else if (input == "Frog")
+                    {
+                        gender = tokens[2];
+                        Frog frog = new Frog(name, age, gender);
+                        animals.Add(frog);
+                    }
+                    else if (input == "Kitten")
+                    {
+                        Kitten kit = new Kitten(name, age);
+                        animals.Add(kit);
+                    }
+                    else if (input == "Tomcat")
+                    {
+                        Tomcat tom = new Tomcat(name, age);
+                        animals.Add(tom);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message); 
+                }
+                
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine(animal);
+                animal.ProduceSound();
+            }
         }
     }
 }
---
+++ 04_C# OOP/InheritanceExercise/Animals/Tomcat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string gender = "Male";
+        public Tomcat(string name, int age, string gender = "Male") : base(name, age, gender)
+        {
+            this.Gender = gender;
+        }
+
+        public override void ProduceSound()
+        {
+            Console.WriteLine("MEOW");
+        }
+    }
+}
