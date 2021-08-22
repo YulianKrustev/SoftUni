ebbb8057164e95f2699602cd865c4a693d55b5a7
YulianKrustev <juliankrustev2018@gmail.com>
2021-08-22 16:13:33 +0300

    new solution

---
+++ 04_C# OOP/EncapsulationExercise/ClassBoxData/Box.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Heigth = height;
+        }
+
+        public double Heigth
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public string SurfaceArea()
+        {
+            double surfaceArea = (2 * length * height) + (2 * length * width) + (2 * height * width);
+
+            return $"Surface Area - {surfaceArea:f2}";
+        }
+
+        public string LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = (2 * length * height) + (2 * width * height);
+
+            return $"Lateral Surface Area - {lateralSurfaceArea:f2}";
+        }
+
+        public string Volume()
+        {
+            double volume = length * height * width;
+
+            return $"Volume - {volume:f2}";
+        }
+
+    }
+}
