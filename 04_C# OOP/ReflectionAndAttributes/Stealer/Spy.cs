725ab5251dc87b04cb1f516d547d5556a68e6261
YulianKrustev <juliankrustev2018@gmail.com>
2021-10-12 22:34:20 +0300

    new solution

--- 04_C# OOP/ReflectionAndAttributes/Stealer/Spy.cs
+++ 04_C# OOP/ReflectionAndAttributes/Stealer/Spy.cs
@@ -72,5 +72,25 @@ namespace Stealer
             
             return sb.ToString().Trim();
         }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            Type classType = Type.GetType(className);
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().Trim();
+        }
     }
 }
