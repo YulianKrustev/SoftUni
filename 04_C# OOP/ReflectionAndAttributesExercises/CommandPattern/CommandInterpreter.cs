fecf16f87038fd22533ff2e21a8a2d22b01dfa95
YulianKrustev <juliankrustev2018@gmail.com>
2021-10-14 17:44:52 +0300

    new solution

---
+++ 04_C# OOP/ReflectionAndAttributesExercises/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : Core.Contracts.ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string commandName = (inputArgs[0] + "Command").ToLower();
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            Type commmandType = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(n => n.Name.ToLower() == commandName);
+
+            if (commmandType == null)
+            {
+                throw new ArgumentException("Invalid command type");
+            }
+
+            ICommand commandInstance = Activator.CreateInstance(commmandType) as ICommand;
+
+            string result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ 04_C# OOP/ReflectionAndAttributesExercises/CommandPattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using System;
+    using Core.Contracts;
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string name = args[0];
+
+            return $"Hello, {name}";
+        }
+    }
+}
