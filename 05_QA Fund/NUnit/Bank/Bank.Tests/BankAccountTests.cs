39e14c7f19e41b0c75545cbbee81456735d2790b
YulianKrustev <juliankrustev2018@gmail.com>
2021-10-19 15:52:53 +0300

    new

--- 05_QA Fund/NUnit/Bank/Bank.Tests/BankAccountTests.cs
+++ 05_QA Fund/NUnit/Bank/Bank.Tests/BankAccountTests.cs
@@ -15,9 +15,59 @@ namespace Bank.Tests
         }
 
         [Test]
-        public void AcountInitializeWithPositiveValue()
+        public void AcountInitializeWithNegativeValue()
         {
-            Assert.AreEqual(2000M, account.Amount);
+            TestDelegate invalidAccount = () => new BankAccount(-500);
+            Assert.Throws<ArgumentException>(invalidAccount);
+        }
+
+        [Test]
+        public void WithdrowWithAmountLessThan1000()
+        {
+            account.Withdraw(500);
+            Assert.AreEqual(1475, account.Amount);
+        }
+
+        [Test]
+        public void WithdrowWithAmountMoreThan1000()
+        {
+            account.Withdraw(1500);
+            Assert.AreEqual(470, account.Amount);
+        }
+
+        [Test]
+        public void WithdrowWithZeroAmount()
+        {
+            TestDelegate invalidWithdrow = () => account.Withdraw(0);
+            Assert.Throws<ArgumentException>(invalidWithdrow);
+        }
+
+        [Test]
+        public void WithdrowWithNegativeAmount()
+        {
+            TestDelegate invalidWithdrow = () => account.Withdraw(-500);
+            Assert.Throws<ArgumentException>(invalidWithdrow);
+        }
+
+        [Test]
+        public void WithdrowWith1000()
+        {
+            account.Withdraw(1000);
+            Assert.AreEqual(980, account.Amount);
+        }
+
+        [Test]
+        public void WithdrowWith0()
+        {
+            TestDelegate invalidWithdrow = () => account.Withdraw(0);
+            Assert.Throws<ArgumentException>(invalidWithdrow);
+        }
+
+        [Test]
+        public void WithdrowWithInvalidAmaount()
+        {
+            TestDelegate invalidWithdrow = () => account.Withdraw(3000);
+            Assert.Throws<ArgumentException>(invalidWithdrow);
         }
     }
 }
