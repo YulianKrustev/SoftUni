cc4f2b21cb4a104c8b89c589aab222ebd0c59f03
YulianKrustev <juliankrustev2018@gmail.com>
2021-11-12 07:56:42 +0200

    unit test

--- 04_C# OOP/UnitTesting/Lab/Tests/AxeTests.cs
+++ 04_C# OOP/UnitTesting/Lab/Tests/AxeTests.cs
@@ -22,12 +22,9 @@ public class AxeTests
     [Test]
     public void AxeLosesDurabilyAfterAttack()
     {
-        Axe axe = new Axe(10, 10);
-        Dummy dummy = new Dummy(10, 10);
-
         axe.Attack(dummy);
 
-        Assert.AreEqual(9, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        Assert.AreEqual(1, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
     }
 
     [Test]
---
+++ 04_C# OOP/UnitTesting/Lab/Tests/DummyTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+
+
+public class DummyTests
+{
+    private Dummy dummy;
+
+    [SetUp]
+    public void TestInit()
+    {
+        dummy = new Dummy(0, 10);
+    }
+
+    [Test]
+    public void DummyLosesHealthAfterAttack()
+    {
+        // Arrange
+        Dummy dummy = new Dummy(20, 10);
+
+        // Act
+        dummy.TakeAttack(5);
+
+        // Assert
+        Assert.IsTrue(dummy.Health == 15);
+    }
+
+    [Test]
+    public void DeadDummyThrowsExceptionIfAttacked()
+    {
+        // Assert
+        Assert.That(() => dummy.TakeAttack(10), Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+    }
+
+    [Test]
+    public void DeadDummyCanGiveXP()
+    {
+        Assert.That(() => dummy.GiveExperience() == 10);
+    }
+
+    [Test]
+    public void AliveDummyCantGiveXP()
+    {
+        Dummy dummy = new Dummy(1, 10);
+
+        Assert.That(() => dummy.GiveExperience() == 0, Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+    }
+}
+
