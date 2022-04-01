2ea1e946b3efcd4e48429493a119b4113e2acd48
YulianKrustev <juliankrustev2018@gmail.com>
2022-04-01 06:11:51 +0300

    upload exam

---
+++ 04_C# OOP/NavalVessels-Skeleton/NavalVessels/Models/Vessel.cs
@@ -0,0 +1,44 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Vessel : Contracts.IVessel
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Vessel name cannot be null or empty.");
+                }
+
+                name = value;
+            }
+        }
+
+        public ICaptain Captain { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double ArmorThickness { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double MainWeaponCaliber => throw new NotImplementedException();
+
+        public double Speed => throw new NotImplementedException();
+
+        public ICollection<string> Targets => throw new NotImplementedException();
+
+        public void Attack(IVessel target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RepairVessel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
