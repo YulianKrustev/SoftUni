5d72cccb7b9b934d418f1fcde2681b0a378a69eb
YulianKrustev <juliankrustev2018@gmail.com>
2022-04-26 08:47:21 +0300

    MSSQL

---
+++ 06_MS Sql Basics/01_Database Introduction/SQLQuery1.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Minions
+
+CREATE TABLE Minions
+(
+	[Id] int primary key,
+	[Name] nvarchar(20),
+	[Age] tinyint 
+)
+
+CREATE TABLE Towns
+(
+	[Id] int primary key,
+	[Name] nvarchar(20)
+)
+
