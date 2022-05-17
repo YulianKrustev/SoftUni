ca852b8b9f0bbed431f12357f1e06aac8a1ab33e
YulianKrustev <juliankrustev2018@gmail.com>
2022-05-17 22:51:34 +0300

    SQL TASKS

--- 06_MS Sql Basics/01_Database Introduction/SQLQuery11.sql
+++ 06_MS Sql Basics/01_Database Introduction/SQLQuery11.sql
@@ -46,5 +46,19 @@ FROM Employees
 
 CREATE VIEW V_EmployeeNameJobTitle
 AS
-SELECT CONCAT(FirstName, ' ', MiddleName, ' ', LastName) AS [Full Name],JobTitle
+SELECT CONCAT(FirstName, ' ', ISNULL(MiddleName, '') + ' ' + LastName) AS [Full Name],JobTitle
 FROM Employees
+
+SELECT DISTINCT JobTitle FROM Employees
+
+SELECT TOP(10) * FROM Projects
+ORDER BY StartDate, [Name]
+
+SELECT TOP(7) FirstName, LastName, HireDate FROM Employees
+ORDER BY HireDate DESC
+
+UPDATE Employees
+SET Salary += Salary * 0.12
+WHERE DepartmentID IN (1, 2, 4, 11)
+
+SELECT Salary FROM Employees
